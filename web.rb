0793b61ca297d554985e53ddfe59237a7e776975
Tony DiNitto <think41c@gmail.com>
2015-03-20 13:55:49 -0600

    Guess checker with form now.

--- web.rb
+++ web.rb
@@ -1,8 +1,21 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(1..100)
+$number = rand(1..100)
 
 get '/' do 
-  erb :index, :locals => {:number => number}
+  # throw params.inspect
+  message = guess_checker(params["guess"].to_i)
+  erb :index, :locals => {:number => $number, 
+                          :message => message
+                         }
+  #guess_checker
+end
+
+def guess_checker(guess)
+  if guess > $number
+    "too high"
+  else 
+    "too low"
+  end
 end
