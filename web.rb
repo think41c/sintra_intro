a017ad45d896ba011b8d2663d9aba727b407dd5b
Tony DiNitto <think41c@gmail.com>
2015-03-23 13:26:46 -0600

    More features implemented.

--- web.rb
+++ web.rb
@@ -1,23 +1,47 @@
 require 'sinatra'
-require 'sinatra/reloader'
+require 'sinatra/fix_951'
+require 'sinatra/reloader' if development? 
 
-$number = rand(1..100)
+@@guesses = 0 
 
-get '/' do 
-  # throw params.inspect
-  message = guess_checker(params["guess"].to_i)
-  erb :index, :locals => {:number => $number, 
-                          :message => message
-                         }
+get '/' do
+  start
+  cheat_code = check_cheat(params["cheat"])
+  guess      = params["guess"].to_i
+  erb :index, :locals => {:number => @@rand, :message => guess_checker(guess, @@rand), :cheater => cheat_code}
 end
 
-def guess_checker(guess)
-  if guess > $number
-    "too high"
-  elsif $number == guess
-    "correct!"
-    "The SECRET NUMBER is <%= number %>"
+def check_cheat(cheat)
+  if cheat == nil 
+    "Not a cheater" 
+  elsif cheat == "true"
+    "The secret #{@@rand}"
+  end
+end
+
+def randz
+  @@rand = rand(101)
+end
+
+def start
+  if @@guesses == 0
+    @@guesses = 1
+    randz
+  elsif @@guesses == 5
+    @@guesses = 1
+    randz
   else 
-    "too low"
+    @@guesses += 1 
+    @@rand
+  end
+end
+
+def guess_checker(guess, random)
+  if guess > random
+    {toney: "Too high", background: "red", count: @@guesses}
+  elsif   random == guess
+    {toney: "Correct. The SECRET NUMBER is (#{random})", background: "blue", count: @@guesses}
+  else
+    {toney: "Too low", background: "yellow", count: @@guesses}
   end
 end
